f8babcc70bbee07e03189acbcedf80617c4626f8
caixiaoshun <cs.shunzhang@foxmail.com>
2025-01-10 15:34:44 +0000

    chore(task1): update training scripts for face segmentation
    - Move swin_base_ours_seg_cls_15_embed_256_cloud_ce_dice_no_attn12.py to CUDA device 3
    - Add new training scripts for transform layer 2 and 3 configurations

--- task1.sh
+++ task1.sh
@@ -1,3 +1,5 @@
 CUDA_VISIBLE_DEVICES=3 python tools/train.py configs/faceseg/swin_base_ours_seg_cls_15_embed_256_cloud_ce_dice_no_attn1.py
 CUDA_VISIBLE_DEVICES=4 python tools/train.py configs/faceseg/swin_base_ours_seg_cls_15_embed_256_cloud_ce_dice_no_attn2.py
-CUDA_VISIBLE_DEVICES=4 python tools/train.py configs/faceseg/swin_base_ours_seg_cls_15_embed_256_cloud_ce_dice_no_attn12.py
+CUDA_VISIBLE_DEVICES=3 python tools/train.py configs/faceseg/swin_base_ours_seg_cls_15_embed_256_cloud_ce_dice_no_attn12.py
+CUDA_VISIBLE_DEVICES=3 python tools/train.py configs/faceseg/swin_base_ours_seg_cls_15_embed_256_cloud_ce_dice_tranform_layer_2.py
+CUDA_VISIBLE_DEVICES=3 python tools/train.py configs/faceseg/swin_base_ours_seg_cls_15_embed_256_cloud_ce_dice_tranform_layer_3.py
