61f3961f2180afd540a3f5147f280f572c6c18d8
caixiaoshun <cs.shunzhang@foxmail.com>
2025-01-13 06:58:22 +0000

    feat(config): add Vaihingen dataset and SwinV2 model configurations
    - Add Vaihingen dataset configuration to configs/_base_/datasets/
    - Create new config for SwinV2 model with OursDecoder for Vaihingen dataset
    - Include data preprocessing and model architecture details

---
+++ configs/faceseg/swinv2_base_ours_seg_cls_15_embed_256_vaihingen_ce_dice.py
@@ -0,0 +1,18 @@
+_base_ = [
+    "../_base_/models/face_seg.py",
+    "../_base_/datasets/vaihingen.py",
+    "../_base_/default_runtime.py",
+    "../_base_/schedules/cloud_scedule.py",
+]
+
+data_preprocessor = dict(size=(512, 512))
+model = dict(
+    data_preprocessor=data_preprocessor,
+    backbone=dict(model="swinv2_base", input_resolution=512),
+    decode_head=dict(
+        type="OursDecoder",
+        token_lens=6,
+        transformer=dict(type="OursTwoWayTransformer", depth=1),
+        num_classes=6, 
+    ),
+)
